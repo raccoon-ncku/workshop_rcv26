d0139a755c63adaf8e31010a843ca2768275bbb4
ChiaChing-Yen <ccyen@umich.edu>
2026-02-24 12:40:35 +0800

    Add git safe.directory configuration and update system dependencies in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,9 @@ set -e
 cd /home/ubuntu/workshop_ws
 source /opt/ros/jazzy/setup.bash
 
+# Fix dubious ownership for git repositories in the workspace
+git config --global --add safe.directory '*'
+
 # 2. Automate X11 Permissions
 xhost +local:root > /dev/null 2>&1 || echo "export DISPLAY=:1" >> ~/.bashrc
 
@@ -16,7 +19,7 @@ echo "--- Installing System Dependencies ---"
 sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt install -y ros-jazzy-moveit-ros-perception ros-jazzy-ros-gz ros-jazzy-gz-ros2-control
+sudo apt install -y ros-jazzy-moveit-ros-perception ros-jazzy-ros-gz ros-jazzy-gz-ros2-control ros-jazzy-qt*
 rosdep update
 rosdep install --from-paths src --ignore-src -y --rosdistro jazzy
 
